f6ff11bc126c89f30a0ec4d69ea87feeba21265b
berk gümüş <63772343+berk420@users.noreply.github.com>
2026-04-24 02:39:03 +0300

    comit
    comit

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL ?? 'https://balinaapi.testprocess.com.tr';
+const API_URL = 'http://balinaapi.testprocess.com.tr';
 
 const api = axios.create({ baseURL: API_URL });
 
